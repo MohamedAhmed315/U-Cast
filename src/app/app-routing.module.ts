3fcb9c9eba47b0257d3c56962f0eda48f8f7bae5
Mohamed Saber <mohamedahmed315@hotmail.com>
2021-04-19 12:27:04 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
     component : AuditionComponent //home page
   },
   {
-    path: "roles",
+    path: "/roles",
     component : RolesComponent
   },
   {
-    path: "feedback",
+    path: "/feedback",
     component: FeedbackComponent
   }
 ];
